3fb05df70c6601112b3488a29bef2ac955a44713
Pawan Singh <63145897+pawanbajeli@users.noreply.github.com>
2024-06-06 16:44:58 +0530

    Update fifoQueue.js

--- server/fifoQueue.js
+++ server/fifoQueue.js
@@ -1,24 +1,72 @@
+// class FIFOQueue {
+//     constructor() {
+//         this.queue = [];
+//     }
+
+//     enqueue(request) {
+//         this.queue.push(request);
+//     }
+
+//     dequeue() {
+//         return this.queue.shift();
+//     }
+
+//     isEmpty() {
+//         return this.queue.length === 0;
+//     }
+
+//     // Add a method to get the size of the queue
+//     size() {
+//         return this.queue.length;
+//     }
+// }
+
+// module.exports = FIFOQueue;
+
+
+/**
+ * Class representing a First In First Out (FIFO) Queue.
+ */
 class FIFOQueue {
+    /**
+     * Create a FIFOQueue.
+     */
     constructor() {
+        // Initialize an empty queue
         this.queue = [];
     }
 
+    /**
+     * Add a request to the end of the queue.
+     * @param {*} request - The request to enqueue.
+     */
     enqueue(request) {
         this.queue.push(request);
     }
 
+    /**
+     * Remove and return the first request from the queue.
+     * @returns {*} - The dequeued request.
+     */
     dequeue() {
         return this.queue.shift();
     }
 
+    /**
+     * Check if the queue is empty.
+     * @returns {boolean} - True if the queue is empty, false otherwise.
+     */
     isEmpty() {
         return this.queue.length === 0;
     }
 
-    // Add a method to get the size of the queue
+    /**
+     * Get the size of the queue.
+     * @returns {number} - The number of elements in the queue.
+     */
     size() {
         return this.queue.length;
     }
 }
 
-module.exports = FIFOQueue;
+module.exports = FIFOQueue; // Export the FIFOQueue class
