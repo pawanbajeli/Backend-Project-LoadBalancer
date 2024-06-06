85ee8cdeb8416d010e1c81bcab26392ddcc86ed3
Pawan Singh <63145897+pawanbajeli@users.noreply.github.com>
2024-06-06 16:52:46 +0530

    Update RoundRobin.js

--- server/RoundRobin.js
+++ server/RoundRobin.js
@@ -1,38 +1,3 @@
-// class RoundRobinQueue {
-//     constructor(endpoints, rotationInterval) {
-//         this.endpoints = endpoints;
-//         this.currentIndex = {};
-//         this.rotationInterval = rotationInterval || 5000; // Default rotation interval of 5 seconds
-
-//         for (const apiType in endpoints) {
-//             this.currentIndex[apiType] = 0;
-//         }
-
-//         // Start rotating the endpoints
-//         setInterval(() => {
-//             this.rotateEndpoints();
-//         }, this.rotationInterval);
-//     }
-
-//     dequeue(apiType) {
-//         const endpoint = this.endpoints[apiType][this.currentIndex[apiType]];
-//         this.currentIndex[apiType] = (this.currentIndex[apiType] + 1) % this.endpoints[apiType].length;
-//         return endpoint;
-//     }
-
-//     rotateEndpoints() {
-//         for (const apiType in this.currentIndex) {
-//             this.currentIndex[apiType] = (this.currentIndex[apiType] + 1) % this.endpoints[apiType].length;
-//         }
-//     }
-
-//     isEmpty() {
-//         return false;
-//     }
-// }
-
-// module.exports = RoundRobinQueue;
-
 
 
 /**
