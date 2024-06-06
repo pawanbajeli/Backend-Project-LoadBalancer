fc5b75a1c8e2c9ca009b188e2de3ce509bf0e42b
Pawan Singh <63145897+pawanbajeli@users.noreply.github.com>
2024-06-06 16:53:58 +0530

    Update fifoQueue.js

--- server/fifoQueue.js
+++ server/fifoQueue.js
@@ -1,27 +1,3 @@
-// class FIFOQueue {
-//     constructor() {
-//         this.queue = [];
-//     }
-
-//     enqueue(request) {
-//         this.queue.push(request);
-//     }
-
-//     dequeue() {
-//         return this.queue.shift();
-//     }
-
-//     isEmpty() {
-//         return this.queue.length === 0;
-//     }
-
-//     // Add a method to get the size of the queue
-//     size() {
-//         return this.queue.length;
-//     }
-// }
-
-// module.exports = FIFOQueue;
 
 
 /**
