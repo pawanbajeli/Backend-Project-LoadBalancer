b2efb661f65b07481d81c26ec90174c0ec3d7253
Pawan Singh <63145897+pawanbajeli@users.noreply.github.com>
2024-06-06 16:55:11 +0530

    Update priorityQueue.js

--- server/priorityQueue.js
+++ server/priorityQueue.js
@@ -1,29 +1,4 @@
-// const ExternalPriorityQueue = require('js-priority-queue');
 
-// class PriorityQueue {
-//     constructor() {
-//         this.queue = new ExternalPriorityQueue({ comparator: (a, b) => a.priority - b.priority });
-//     }
-
-//     enqueue(request) {
-//         this.queue.queue(request);
-//     }
-
-//     dequeue() {
-//         return this.queue.dequeue();
-//     }
-
-//     isEmpty() {
-//         return this.queue.length === 0;
-//     }
-
-    
-//     size() {
-//         return this.queue.length;
-//     }
-// }
-
-// module.exports = PriorityQueue;
 
 const ExternalPriorityQueue = require('js-priority-queue'); // Import the js-priority-queue module
 
