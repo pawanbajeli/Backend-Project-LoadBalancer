9e0efc8b797cdcb0dd388996d83b6f97bee9c6a4
Pawan Singh <63145897+pawanbajeli@users.noreply.github.com>
2024-06-06 16:54:52 +0530

    Update logger.js

--- server/logger.js
+++ server/logger.js
@@ -1,34 +1,4 @@
-// const winston = require('winston');
 
-// const logger = winston.createLogger({
-//     level: 'info',
-//     format: winston.format.json(),
-//     defaultMeta: { service: 'load-balancer' },
-//     transports: [
-//         new winston.transports.File({ filename: 'server/logs/requests.log' })
-//     ],
-// });
-
-// module.exports = logger;
-
-
-// const winston = require('winston');
-// const path = require('path');
-
-// // Define the log directory
-// const logDirectory = path.join(__dirname, 'logs');
-
-// // Create the logger
-// const logger = winston.createLogger({
-//     level: 'info',
-//     format: winston.format.json(),
-//     defaultMeta: { service: 'load-balancer' },
-//     transports: [
-//         new winston.transports.File({ filename: path.join(logDirectory, 'requests.log') })
-//     ],
-// });
-
-// module.exports = logger;
 
 const winston = require('winston'); // Import the Winston logging library
 const path = require('path'); // Import the path module
